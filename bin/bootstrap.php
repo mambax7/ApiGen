be92114dee0a6b6c69f5937f7aa0595e59277b94
Vítězslav Dvořák <info@vitexsoftware.cz>
2020-08-10 09:49:07 +0200

    Backage built

--- bin/bootstrap.php
+++ bin/bootstrap.php
@@ -1,20 +1,26 @@
-<?php declare(strict_types=1);
+<?php
 
-$rootDir = realpath(__DIR__ . '/../');
+declare(strict_types=1);
 
+if (strlen(Phar::running()) > 0 ? true : false) {
+    $autoloader = './vendor/autoload.php';
+} else {
+    $rootDir = realpath(__DIR__ . '/../');
 // Autoloader for standalone install (installed via `composer create-project` or cloned locally)
-$autoloader = realpath($rootDir . '/vendor/autoload.php');
+    $autoloader = realpath($rootDir . '/vendor/autoload.php');
+    if (!$autoloader) {
+// Installed via `composer [global] require`.
+        $autoloader = realpath($rootDir . '/../../autoload.php');
+    }
 
-if (! $autoloader) {
-    // Installed via `composer [global] require`.
-    $autoloader = realpath($rootDir . '/../../autoload.php');
+    if (!$autoloader) {
+        throw new RuntimeException(
+                'ApiGen was unable to find its autoloader. ' .
+                'Did you forget to run "composer update"?'
+        );
+    }
 }
 
-if (! $autoloader) {
-    throw new RuntimeException(
-        'ApiGen was unable to find its autoloader. ' .
-        'Did you forget to run "composer update"?'
-    );
-}
+
 
 require_once $autoloader;
